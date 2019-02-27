7ea685493c3d6393d92f8fa5206b8f73322b7f04
Nompumelelo <32453496+elolelo@users.noreply.github.com>
2019-02-27 19:50:07 +0200

    added an indexOf() method
    This method is going to loop through the array and search for the elements specified in the index

--- Main.java
+++ Main.java
@@ -4,8 +4,33 @@ public class Main {
 	// constants
 	static final char[] suits = {'H', 'D', 'C', 'S'};  
     	static final String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"}; 
+    	
+    	
+    	//this indexOf method is going to be useful for the checkings:
+    	
+    	public static <T> int indexOf(T[] haystack, T needle)
+    {
+        for (int i=0; i<haystack.length; i++)
+        {
+            if (haystack[i].equals(needle)) return i;   // this returns that element if found
+        }
 
+        return -1;     // if the element is not found -1 is returned
+    }
 
+	
+    public static int indexOf(char[] haystack, char needle)     // same method but different arguments
+    {
+        for (int i=0; i<haystack.length; i++)     
+        {
+            if (haystack[i] == needle) return i;
+        }
+
+        return -1;
+    }
+  
+
+	// this method is for getting and validating user input
 	public static List<String> getCardsFromInput() {  
       	
       	Scanner sc = new Scanner(System.in);
@@ -24,6 +49,7 @@ public class Main {
 		}
 	return inputCards;
 	}
+	
 	
 }
  
