08936291a2c6932b2647c043cbd25210e0ced45a
Nompumelelo <32453496+elolelo@users.noreply.github.com>
2019-02-27 18:13:10 +0200

    User inputs

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main { 
+
+
+	public static List<String> getCardsFromInput() {
+      	
+      	Scanner sc = new Scanner(System.in);
+      		
+      	List<String> inputCards = new ArrayList<String>();  
+	while (inputCards.size() < 5) {
+        String cardString = sc.nextLine();
+        System.out.println("input: " + cardString);
+
+        if (cardString.length() != 2) {
+          // "2H", "AS"
+          System.out.println("Invalid card: " + cardString);
+          continue;
+        }
+
+
+ }
+
