d5ffbfa80b930558c7a50deaf773bcf1f544f6bc
Nompumelelo <32453496+elolelo@users.noreply.github.com>
2019-02-27 21:34:20 +0200

    Added the class card, a method buildHand
    For the buildHand method - I used of the best  features of Java8

--- Main.java
+++ Main.java
@@ -1,24 +1,23 @@
 import java.util.*;
 public class Main { 
 
-	// constants
+	// constants suits and ranks
 	static final char[] suits = {'H', 'D', 'C', 'S'};  
     	static final String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"}; 
     	
     	
-    	//this indexOf method is going to be useful for the checkings:
-    	
+    	//this indexOf method is useful for ranks
     	public static <T> int indexOf(T[] haystack, T needle)
-    {
+    	{
         for (int i=0; i<haystack.length; i++)
         {
             if (haystack[i].equals(needle)) return i;   // this returns that element if found
         }
-
         return -1;     // if the element is not found -1 is returned
-    }
+    	}
 
 	
+	//this indexOf method is useful for suits	
     public static int indexOf(char[] haystack, char needle)     // same method but different arguments
     {
         for (int i=0; i<haystack.length; i++)     
@@ -50,7 +49,58 @@ public class Main {
 	return inputCards;
 	}
 	
+	static class Card {     // this class is represents a card
+        final int suit;
+        final int rank;
+
+        Card(int s, int r) {   
+            suit = s;
+            rank = r;
+        }
+
+        public String rankString() {
+          return ranks[rank];
+        }
+
+        public String suitString() {
+          return suits[suit] + "";
+        }
+
+        @Override
+        public String toString() {
+            return  ranks[rank] + suits[suit]; 
+        }
+    }
 	
+	
+	static List<Card> buildHand(List<String> cardStrings) {  // this method builds a string "card" from user inputs
+      		List<Card> hand = new ArrayList<Card>();    // this arraylist is for the comparison
+      		for (String cardString : cardStrings) {
+			String sRank = cardString.charAt(0) + "";
+			char cSuit = cardString.charAt(1);
+			int r = indexOf(ranks, sRank);
+			int s = indexOf(suits, cSuit);
+
+		  if (r == -1 || s == -1) {
+		    System.out.println("Invalid card: " + cardString);  // checks for the validity of the input
+		    continue;
+          }
+
+		  Card card = new Card(s, r);
+		  hand.add(card);
+        }
+        Collections.sort(hand, Comparator.comparing(c -> c.rank));   // best Java 8 feature!
+        return hand;
+    }
+	
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Please enter your cards (one by one): ");
+		List<String> inputCards = getCardsFromInput();
+		//runTests();
+	}
 }
  
 
