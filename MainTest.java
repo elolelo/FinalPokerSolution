7ab00ce5f4a289e7705c3e117eb8d3595de53e2e
Nompumelelo <32453496+elolelo@users.noreply.github.com>
2019-03-01 15:33:01 +0200

    All failed methods testing

---
+++ MainTest.java
@@ -0,0 +1,52 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testIndexOfTArrayT() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testIndexOfCharArrayChar() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGenerateDeck() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGenerateHand() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetCardsFromInput() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testBuildHand() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testValue() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testFindRuns() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
