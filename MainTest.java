3f9204f0a2464a406f41ed9622320a4b90308686
Nompumelelo <32453496+elolelo@users.noreply.github.com>
2019-03-01 18:29:36 +0200

    Testing the generateDeck method  -- failed test
    input of 55

--- MainTest.java
+++ MainTest.java
@@ -1,13 +1,20 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class MainTest {
 
+	static final char[] suits = {'H', 'D', 'C', 'S'};
+	static final String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
+	
 	@Before
 	public void setUp() throws Exception {
-		testIndexOfTArrayT();
+
 	}
 
 	@Test
@@ -23,6 +30,8 @@ public class MainTest {
 	@Test
 	public void testGenerateDeck() {
 		//fail("Not yet implemented"); // TODO
+		List<Main.Card> deck = Main.generateDeck();
+		assertEquals(55,deck.size());
 	}
 
 	@Test
@@ -39,15 +48,17 @@ public class MainTest {
 	public void testBuildHand() {
 		//fail("Not yet implemented"); // TODO
 	}
-
+	
 	@Test
 	public void testValue() {
 		//fail("Not yet implemented"); // TODO
+		//Should test all cases: Full House, Flush, etc
 	}
 
 	@Test
 	public void testFindRuns() {
 		//fail("Not yet implemented"); // TODO
+		//Should test all cases: Full House, Flush, etc
 	}
 
 }
